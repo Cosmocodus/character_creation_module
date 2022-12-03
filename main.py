b92b36175991d8e8863e053853400301fb1056bf
Andrei Kaplin~ <kaplergt@gmail.com>
2022-12-03 18:42:44 +0300

    Code style2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -77,8 +79,8 @@ def choice_char_class() -> str:
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
 
-
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -89,5 +91,5 @@ def main():
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
     
-
+    
 main()
